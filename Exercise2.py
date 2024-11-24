16eec6e925f3c2934aab1579296ba5542a2a6dbc
Harneet Kour <harneetkour0280@gmail.com>
2024-11-24 18:05:28 +0400

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,6 @@
+#In this exercise, you will create and work with integer variables, perform arithmetic operations, and print the result to the console.
+a = 8 #adds value to variable a
+b = 10 #adds value to variable b
+sum = a + b #calculates the sum of a + b and adds the result to sum
+print(sum) 
+#In the code above, two variables have been declared with another variable to store the sum of the two and then outputed with the result.
