4ca63f1ce336e8340abd38231ce5ccbf91c64161
Harneet Kour <harneetkour0280@gmail.com>
2024-11-24 18:05:20 +0400

    Create Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,6 @@
+#Fill in the blanks in the Python code below to output the phrase "Coding is Cool" to the console using variables and string concatenation.
+word1 = ("Coding")
+word2 = ("is")
+word3 = ("Cool")
+print(f"{word1} {word2} {word3}") #brings the words together to form the needed output
+#In the code above, the empty blanks from the question has been filled to output "Coding is Cool".
