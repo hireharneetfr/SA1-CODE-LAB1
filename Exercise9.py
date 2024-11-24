730a866edae4a6d29d84d2a6e3abc9217a0eec8a
Harneet Kour <harneetkour0280@gmail.com>
2024-11-24 18:05:57 +0400

    Create Exercise9.py

---
+++ Exercise9.py
@@ -0,0 +1,8 @@
+#Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
+def hello():
+    print("Hello")
+def main():
+    hello()
+if __name__ == "__main__":
+    main()
+#In the above code, the blanks have been filled for it to output "Hello"
