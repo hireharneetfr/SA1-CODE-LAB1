75579a1cd5f45a4ac178f9fe7b957f3df399c4dc
Harneet Kour <harneetkour0280@gmail.com>
2024-11-24 18:05:46 +0400

    Create Exercise7.py

---
+++ Exercise7.py
@@ -0,0 +1,21 @@
+#Use your newly acquired knowledge of the for loop to complete the following tasks. Print all values to the console in each case.
+print("counts up from 0 to 50 in increments of 1: ")
+for num in range(0, 51, 1):
+    print(num)
+print()
+print("counts down from 50 to 0 in decrements of 1: ")
+for num in range(50, -1, -1):
+    print(num)
+print()
+print("counts up from 30 to 50 in increments of 1: ")
+for num in range(30, 51, 1):
+    print(num)
+print()
+print("counts down from 50 to 10 in decrements of 2: ")
+for num in range(50, 9, -2):
+    print(num)
+print()
+print("counts up from 100 to 200 in increments of 5")
+for num in range(100, 201, 5):
+    print(num)
+#In the above code, Increments and decrement are done for the requested numbers.    
