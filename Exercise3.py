16c0c9dbfa6e8aad30ae23d0e2ef063b2454fb69
Harneet Kour <harneetkour0280@gmail.com>
2024-11-24 18:05:34 +0400

    Create Exercise3.py

---
+++ Exercise3.py
@@ -0,0 +1,10 @@
+#In this exercise, you'll create a program that stores and prints your name, hometown, and age to the console using a Python dictionary.
+name = input("type your first name: ") #asks user for its first name
+name2 = input("type in your second name: ") #asks user for its second name
+home = input("type your hometown: ") #asks user for its hometown
+age = int(input("type your age: ")) #asks user for its age
+print()
+print(f"Your name is {name} {name2}.") #outputs the users complete name by joining first and second name
+print(f"Your hometown is {home}.") #outputs the hometown of the user
+print(f"Your age is {age}.") #outputs the age of the user
+#In the code above, it asks for the name, home and age from the user and outputs the same. 
